9e2501b2c4868d5d0a6a8d46d30fffa54229ccdf
cta70 <calvin_truong@sfu.ca>
2021-09-28 00:31:34 -0700

    created dummy monthly records model for backend

--- hha_prj/monthly_records/models.py
+++ hha_prj/monthly_records/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class MonthlyRecord(models.Model):
+    description = models.CharField(max_length=100, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
