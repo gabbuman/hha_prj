64954a86181004c77a0925ddac8913ed97c6a001
shubhmall <skmall@sfu.ca>
2021-09-30 13:28:01 -0700

    Added Rehab table in database with Red Field Points

--- hha_prj/monthly_records/models.py
+++ hha_prj/monthly_records/models.py
@@ -4,3 +4,21 @@ from django.db import models
 class MonthlyRecord(models.Model):
     description = models.CharField(max_length=100, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+class RehabMonthlyRecord(models.Model):
+    beds_available = models.PositiveIntegerField()
+    bed_days = models.PositiveIntegerField()
+    patient_days = models.PositiveIntegerField()
+    hospitalized = models.PositiveIntegerField()
+    discharged_alive = models.PositiveIntegerField()
+    died_before_48_h = models.PositiveIntegerField()
+    died_after_48_h = models.PositiveIntegerField()
+    days_hospitalized = models.PositiveIntegerField()
+    referrals = models.PositiveIntegerField()
+    transfers = models.PositiveIntegerField()
+    self_discharged = models.PositiveIntegerField()
+    stayed_in_the_ward = models.PositiveIntegerField()
+    admissions = models.PositiveIntegerField()
+
+    def __str__(self):
+        return 'Rehab Record'
