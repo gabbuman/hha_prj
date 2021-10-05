e8f1266e186229405421d187a59ba05fcbf58a26
vaibhavs <vaibhavs@sfu.ca>
2021-10-04 22:33:49 -0700

    Update models.py

--- hha_prj/backend/models.py
+++ hha_prj/backend/models.py
@@ -4,3 +4,19 @@ from django.db import models
 class MonthlyRecord(models.Model):
     description = models.CharField(max_length=100, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+class NICUPaeds(models.Model):
+    month = models.CharField(blank=False,auto_created=True)
+    Beds_available = models.IntegerField(blank=True)
+    Bed_days = models.IntegerField(blank=True)
+    Patient_days = models.IntegerField(blank=True)
+    Hospitalized = models.IntegerField(blank=True)
+    Discharged_alive = models.IntegerField(blank=True)
+    Died_before_48h = models.IntegerField(blank=True)
+    Died_after_48h = models.IntegerField(blank=True)
+    Days_hospitalised = models.IntegerField(blank=True)
+    Referrals = models.IntegerField(blank=True)
+    Transfers = models.IntegerField(blank=True)
+    Self_discharged = models.IntegerField(blank=True)
+    Stayed_ward = models.IntegerField(blank=True)
+    Admissions = models.IntegerField(blank=True)
