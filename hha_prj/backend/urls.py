64c2121f89456aa6046e8367abed11048ef635c8
vaibhavs <vaibhavs@sfu.ca>
2021-10-04 22:47:34 -0700

    Update urls.py

--- hha_prj/backend/urls.py
+++ hha_prj/backend/urls.py
@@ -3,5 +3,5 @@ from .api import MonthlyRecordViewSet
 
 router = routers.DefaultRouter()
 router.register('api/monthly_records', MonthlyRecordViewSet, 'monthly_records')
-
+router.register('api/nicu_paed', NICUPaedsViewSet, 'nicu_paed')
 urlpatterns = router.urls
