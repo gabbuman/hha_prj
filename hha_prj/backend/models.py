77ed6db89bac7a605d4f90eba7777c9f6820bb92
vaibhavs <vaibhavs@sfu.ca>
2021-10-04 22:42:24 -0700

    Add year field to models.py

--- hha_prj/backend/models.py
+++ hha_prj/backend/models.py
@@ -7,6 +7,7 @@ class MonthlyRecord(models.Model):
 
 class NICUPaeds(models.Model):
     month = models.CharField(blank=False,auto_created=True)
+    year = models.CharField(blank=False,auto_created=True)
     Beds_available = models.IntegerField(blank=True)
     Bed_days = models.IntegerField(blank=True)
     Patient_days = models.IntegerField(blank=True)
