a5c6e5f21ce8e486b6fdb7d4ab55e45ff71c3a48
cta70 <calvin_truong@sfu.ca>
2021-10-06 00:57:28 -0700

    Copied list template, added arrows and link api to list item

--- hha_prj/frontend/src/components/home/CSPreview.tsx
+++ hha_prj/frontend/src/components/home/CSPreview.tsx
@@ -1,13 +1,91 @@
-import React, { Component } from 'react';
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import ArrowForward from '@mui/icons-material/ArrowForward';
+import Link from '@mui/material/Link';
 
-export class CSPreview extends Component {
-    render() {
-		return (
-            <div>
-
-            </div>
-        )
-    }
+export default function AlignItemsList() {
+  return (
+    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+        <Link href="#" underline="none">
+            <ListItem alignItems="center">
+                <ListItemAvatar>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                </ListItemAvatar>
+                <ListItemText
+                primary="Successful baby delivery"
+                secondary={
+                    <React.Fragment>
+                    <Typography
+                        sx={{ display: 'inline' }}
+                        component="span"
+                        variant="body2"
+                        color="text.primary"
+                    >
+                        NICU
+                    </Typography>
+                    {" — I'll be in your neighborhood doing errands this…"}
+                    </React.Fragment>
+                }
+                />
+                <ArrowForward/>
+            </ListItem>
+        </Link>
+      <Divider variant="inset" component="li" />
+      <Link href="#" underline="none">
+        <ListItem alignItems="center">
+            <ListItemAvatar>
+            <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+            primary="Summer BBQ"
+            secondary={
+                <React.Fragment>
+                <Typography
+                    sx={{ display: 'inline' }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                >
+                    to Scott, Alex, Jennifer
+                </Typography>
+                {" — Wish I could come, but I'm out of town this…"}
+                </React.Fragment>
+            }
+            />
+            <ArrowForward/>
+        </ListItem>
+      </Link>
+      <Divider variant="inset" component="li" />
+      <Link href="#" underline="none">
+        <ListItem alignItems="center">
+            <ListItemAvatar>
+            <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+            primary="Oui Oui"
+            secondary={
+                <React.Fragment>
+                <Typography
+                    sx={{ display: 'inline' }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                >
+                    Sandra Adams
+                </Typography>
+                {' — Do you have Paris recommendations? Have you ever…'}
+                </React.Fragment>
+            }
+            />
+            <ArrowForward/>
+        </ListItem>
+      </Link>
+    </List>
+  );
 }
-
-export default CSPreview
