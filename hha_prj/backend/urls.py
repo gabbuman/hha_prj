ea2a0ccb82b3a07494fcc07ef13f6dadd8dc65da
cta70 <calvin_truong@sfu.ca>
2021-10-08 18:04:33 -0700

    added obtain token pair view and standard jwt refresh view to url patterns alongside viewsets

--- hha_prj/backend/urls.py
+++ hha_prj/backend/urls.py
@@ -1,9 +1,16 @@
 from rest_framework import routers
-from .api import MonthlyRecordViewSet, RehabMonthlyRecordViewset, MaternityMonthlyRecordViewSet
+from .api import MonthlyRecordViewSet, RehabMonthlyRecordViewset, MaternityMonthlyRecordViewSet, CustomUserViewSet
+from django.urls import path
+from django.urls.conf import include
+from rest_framework_simplejwt import simple_jwt_views 
 
 router = routers.DefaultRouter()
 router.register('api/monthly_records', MonthlyRecordViewSet, 'monthly_records')
 router.register('api/rehab_records', RehabMonthlyRecordViewset, 'rehab_record')
 router.register('api/maternity', MaternityMonthlyRecordViewSet, 'maternity')
+router.register('api/user', CustomUserViewSet, 'user')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api/token/refresh', simple_jwt_views.TokenRefresh.as_view()),
+]
