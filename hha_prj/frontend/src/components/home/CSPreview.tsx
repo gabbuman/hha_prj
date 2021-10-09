d42dce6561b9503309c1a1165d8a761863508034
cta70 <calvin_truong@sfu.ca>
2021-10-08 23:45:37 -0700

    rewrote cspreview as a react component

--- hha_prj/frontend/src/components/home/CSPreview.tsx
+++ hha_prj/frontend/src/components/home/CSPreview.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Component } from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import Divider from '@mui/material/Divider';
@@ -8,76 +9,79 @@ import Typography from '@mui/material/Typography';
 import ArrowForward from '@mui/icons-material/ArrowForward';
 import Card from '@mui/material/Card';
 
-export default function AlignItemsList() {
-  return (
-    <Card sx={{ maxWidth: 350, boxShadow:3}}>
-        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-            <ListItem alignItems="center">
-                <ListItemButton>
-                    <ListItemText
-                    primary="Patient Story"
-                    secondary={
-                        <React.Fragment>
-                        <Typography
-                            sx={{ display: 'inline' }}
-                            component="span"
-                            variant="body2"
-                            color="text.primary"
-                        >
-                            Maternity
-                        </Typography>
-                        {" — Lorem ipsum dolor sit amet, consectetur adipiscing elit..."}
-                        </React.Fragment>
-                    }
-                    />
-                    <ArrowForward style={{fill: "#B46FBC"}}/>
-                </ListItemButton>
-            </ListItem>
-            <Divider component="li" />
-            <ListItem alignItems="center">
-                <ListItemButton>
-                    <ListItemText
-                    primary="Staff Recognition"
-                    secondary={
-                        <React.Fragment>
-                        <Typography
-                            sx={{ display: 'inline' }}
-                            component="span"
-                            variant="body2"
-                            color="text.primary"
-                        >
-                            Rehab
-                        </Typography>
-                        {" — Lorem ipsum dolor sit amet, consectetur adipiscing elit..."}
-                        </React.Fragment>
-                    }
-                    />
-                    <ArrowForward style={{fill: "#B46FBC"}}/>
-                </ListItemButton>
-            </ListItem>
-            <Divider component="li" />
-            <ListItem alignItems="center">
-                <ListItemButton>
-                    <ListItemText
-                    primary="Equiptment Recieved"
-                    secondary={
-                        <React.Fragment>
-                        <Typography
-                            sx={{ display: 'inline' }}
-                            component="span"
-                            variant="body2"
-                            color="text.primary"
-                        >
-                            NICU
-                        </Typography>
-                        {' — Lorem ipsum dolor sit amet, consectetur adipiscing elit...'}
-                        </React.Fragment>
-                    }
-                    />
-                    <ArrowForward style={{fill: "#B46FBC"}}/>
-                </ListItemButton>
-            </ListItem>
-        </List>
-    </Card>
-  );
+export class CSPreview extends Component {
+    render (){
+        return(
+            <Card sx={{ maxWidth: 350, boxShadow:3}}>
+                <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                    <ListItem alignItems="center">
+                        <ListItemButton>
+                            <ListItemText
+                            primary="Patient Story"
+                            secondary={
+                                <React.Fragment>
+                                <Typography
+                                    sx={{ display: 'inline' }}
+                                    component="span"
+                                    variant="body2"
+                                    color="text.primary"
+                                >
+                                    Maternity
+                                </Typography>
+                                {" — Lorem ipsum dolor sit amet, consectetur adipiscing elit..."}
+                                </React.Fragment>
+                            }
+                            />
+                            <ArrowForward style={{fill: "#B46FBC"}}/>
+                        </ListItemButton>
+                    </ListItem>
+                    <Divider component="li" />
+                    <ListItem alignItems="center">
+                        <ListItemButton>
+                            <ListItemText
+                            primary="Staff Recognition"
+                            secondary={
+                                <React.Fragment>
+                                <Typography
+                                    sx={{ display: 'inline' }}
+                                    component="span"
+                                    variant="body2"
+                                    color="text.primary"
+                                >
+                                    Rehab
+                                </Typography>
+                                {" — Lorem ipsum dolor sit amet, consectetur adipiscing elit..."}
+                                </React.Fragment>
+                            }
+                            />
+                            <ArrowForward style={{fill: "#B46FBC"}}/>
+                        </ListItemButton>
+                    </ListItem>
+                    <Divider component="li" />
+                    <ListItem alignItems="center">
+                        <ListItemButton>
+                            <ListItemText
+                            primary="Equiptment Recieved"
+                            secondary={
+                                <React.Fragment>
+                                <Typography
+                                    sx={{ display: 'inline' }}
+                                    component="span"
+                                    variant="body2"
+                                    color="text.primary"
+                                >
+                                    NICU
+                                </Typography>
+                                {' — Lorem ipsum dolor sit amet, consectetur adipiscing elit...'}
+                                </React.Fragment>
+                            }
+                            />
+                            <ArrowForward style={{fill: "#B46FBC"}}/>
+                        </ListItemButton>
+                    </ListItem>
+                </List>
+            </Card>
+        )
+    }
+
 }
