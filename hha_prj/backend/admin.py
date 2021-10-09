a1f2abdd3fca97f2c3a782536497060091b880a7
cta70 <calvin_truong@sfu.ca>
2021-10-08 17:48:48 -0700

    added fieldsets for admin view and registered customer user admin

--- hha_prj/backend/admin.py
+++ hha_prj/backend/admin.py
@@ -6,10 +6,25 @@ from .forms import CustomUserChangeForm, CustomUserCreationForm
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    change_form = CustomUserCreationForm
+    change_form = CustomUserChangeForm
     create_form = CustomUserCreationForm
+
+    ordering = ('username',)
+    search_fields = ('username',)
 
     # What is displayed on the admin change list page
     list_display = ('username', 'is_staff', 'is_active')
     list_filter = ('username', 'is_staff', 'is_active')
-    
+
+    change_fieldsets = (
+        (None, {('username', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    create_fieldsets = (
+        (None, {
+            'classes': ('collapse', 'extrapretty'),
+            'fields': ('username','password', 'is_staff', 'is_active')
+        })
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
