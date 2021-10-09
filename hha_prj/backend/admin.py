2146571d475209db097a8ebc9a184c9daa48cdac
cta70 <calvin_truong@sfu.ca>
2021-10-08 17:40:07 -0700

    created custom user admin and configured what is displayed in the admin change list page

--- hha_prj/backend/admin.py
+++ hha_prj/backend/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    change_form = CustomUserCreationForm
+    create_form = CustomUserCreationForm
+
+    # What is displayed on the admin change list page
+    list_display = ('username', 'is_staff', 'is_active')
+    list_filter = ('username', 'is_staff', 'is_active')
+    
