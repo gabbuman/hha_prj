55f7f57afbed883b22c61973b3e8ccbce6df12e8
cta70 <calvin_truong@sfu.ca>
2021-10-08 17:05:44 -0700

    added user creation function to user serializer

--- hha_prj/backend/serializers.py
+++ hha_prj/backend/serializers.py
@@ -43,3 +43,13 @@ class CustomUserSerializer(serializers.ModelSerializer):
             "password": {"write_only": True},
         }
 
+    def create(self, validated_data):
+        password = validated_data.pop("password", None)
+        instance = self.Meta.model(
+            **validated_data
+        )
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        
+        return instance
