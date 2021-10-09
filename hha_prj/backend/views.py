50e7552a5e6c0304dda8c805843d3b411cb5ac8a
cta70 <calvin_truong@sfu.ca>
2021-10-08 17:17:11 -0700

    created a token pair view and applied the token pair serializer to it

--- hha_prj/backend/views.py
+++ hha_prj/backend/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import permissions
+from .serializers import CustomTokenPairSerializer
 
 # Create your views here.
+class ObtainTokenPairWithUsernameView(TokenObtainPairView):
+    permission_classes = (permissions.AllowAny)
+    serializer_class = CustomTokenPairSerializer
