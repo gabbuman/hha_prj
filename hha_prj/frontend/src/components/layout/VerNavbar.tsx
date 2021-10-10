1f414d9633a65fcac2866255589da57f2f82c7ef
JinTao <jza237@sfu.ca>
2021-10-10 16:19:50 -0700

    finshed up the navbar and removed files no longer in use

--- hha_prj/frontend/src/components/layout/VerNavbar.tsx
+++ hha_prj/frontend/src/components/layout/VerNavbar.tsx
@@ -1,59 +1,111 @@
-import React, {useState} from 'react'
-import styled from 'styled-components'
-import {Link} from 'react-router-dom'
-import * as FaIcons from 'react-icons/fa'
-import * as AiIcons from 'react-icons/ai'
-import { SecondSideBarData, SidebarData } from './SidebarData'
-import Submenu from './Submenu'
-import Menu from './Menu'
-
-const Icons = styled(Link)`
-    margin-left: 2rem;
-    font-size: 2rem;
-    height: flex;
-    justify-content: flex-start;
-    align-items: center; 
-`;
-
-const SideBarNav = styled.nav`
-    background: #15171c;
-    width: 250px;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    position: fixed;
-    top: 10; 
-    border-right: 1px solid #D7D8DC;
-
-`;
-
-const SideBarWrap = styled.nav`
-    background: #FAF8F8;
-    width: 250px;
-
-`;
-const Sidebar = () => {
-    
-    return (
-        <>
-          <SideBarNav>
-              <SideBarWrap>
-                <Icons to = "#">
-                </Icons>
-                {SidebarData.map((item, index) => {
-                    return <Submenu item={item} key={index} />;
-                })}
-
-                {SecondSideBarData.map((item, index) =>{
-                    return <Menu item={item} key={index} />;
-                })}
-              </SideBarWrap>
-          </SideBarNav>  
-        </>
-    )
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import { makeStyles } from "@material-ui/core/styles";
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
 }
 
-export default Sidebar
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
 
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
 
+interface LinkTabProps {
+    label?: string;
+    href?: string;
+}
 
+function LinkTab(props: LinkTabProps) {
+    return (
+      <Tab
+        component="a"
+        onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+          event.preventDefault();
+        }}
+        {...props}
+      />
+    );
+  }
+
+function a11yProps(index: number) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+    tabs: {
+      "& .MuiTab-wrapper": {
+        flexDirection: "row",
+        justifyContent: "flex-start"
+      }
+    }
+  }));
+
+export default function VerticalTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+      
+    <Box
+      sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 1000 }}
+    >
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        sx={{ borderRight: 1, borderColor: 'divider'}}
+
+      >
+        <Button sx={{justifyContent: 'flex-start', textTransform: 'none'}} variant="outlined" href="/dptcard">
+            Back to Home
+        </Button>
+        <Tab sx={{alignItems: 'flex-start', textTransform: 'none'}} label="Monthly Records" {...a11yProps(1)} />
+        <Tab sx={{alignItems: 'flex-start', textTransform: 'none'}} label="Biomechanical Support" {...a11yProps(2)} />
+        <Tab sx={{alignItems: 'flex-start', textTransform: 'none'}} label="Employee Of the Month" {...a11yProps(3)} />
+        <Tab sx={{alignItems: 'flex-start', textTransform: 'none'}} label="Case Study" {...a11yProps(4)} />
+
+      </Tabs>
+      <TabPanel  value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        Item Four
+      </TabPanel>
+      <TabPanel value={value} index={4}>
+        Item Five
+      </TabPanel>
+    </Box>
+  );
+}
