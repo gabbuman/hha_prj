103a350c888fbdb03241eccffd5e0f81b37fb87d
Jasmeen <jka225@sfu.ca>
2021-10-09 23:02:26 -0700

    Added CommunityHealthModel to Backend and created a serializer for it

--- hha_prj/backend/serializers.py
+++ hha_prj/backend/serializers.py
@@ -1,6 +1,6 @@
 from backend.models import RehabMonthlyRecord
 from rest_framework import serializers
-from backend.models import MaternityMonthlyRecord, MonthlyRecord
+from backend.models import MaternityMonthlyRecord, MonthlyRecord, CommunityHealthRecord
 
 #Monthly Record Serializer
 class MonthlyRecordSerializer(serializers.ModelSerializer):
@@ -18,4 +18,10 @@ class RehabMonthlyRecordSerializer(serializers.ModelSerializer):
 class MaternityMonthlyRecordSerializer(serializers.ModelSerializer):
     class Meta:
         model = MaternityMonthlyRecord
-        fields = '__all__'         
+        fields = '__all__'     
+
+# Community health Record Serializer
+class CommunityHealthRecordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CommunityHealthRecord
+        fields = '__all__'   
