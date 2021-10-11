bec19f2708897dcf4f612773ab6d064823b29a3c
JinTao <jza237@sfu.ca>
2021-10-10 21:56:56 -0700

    re added the vertical bar component

--- hha_prj/frontend/src/components/layout/VerNavbar.tsx
+++ hha_prj/frontend/src/components/layout/VerNavbar.tsx
@@ -1,13 +1,111 @@
-import React, { Component } from 'react';
-
-export class VerNavbar extends Component {
-    render() {
-		return (
-            <div>
-
-            </div>
-        )
-    }
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import { makeStyles } from "@material-ui/core/styles";
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
 }
 
-export default VerNavbar
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+interface LinkTabProps {
+    label?: string;
+    href?: string;
+}
+
+function LinkTab(props: LinkTabProps) {
+    return (      
+    <Tab
+        component="a"
+        onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+          event.preventDefault();
+        }}
+        {...props}
+      />
+    );
+  }
+
+function a11yProps(index: number) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+    tabs: {
+      "& .MuiTab-wrapper": {
+        flexDirection: "row",
+        justifyContent: "flex-start"
+      }
+    }
+  }));
+
+export default function VerticalTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+      
+    <Box
+      sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 1000 }}
+    >
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        sx={{ borderRight: 1, borderColor: 'divider'}}
+
+      >
+        <Button sx={{justifyContent: 'flex-start', textTransform: 'none'}} variant="outlined" href="/dptcard">
+            Back to Home
+        </Button>
+        <Tab sx={{alignItems: 'flex-start', textTransform: 'none'}} label="Monthly Records" {...a11yProps(1)} />
+        <Tab sx={{alignItems: 'flex-start', textTransform: 'none'}} label="Biomechanical Support" {...a11yProps(2)} />
+        <Tab sx={{alignItems: 'flex-start', textTransform: 'none'}} label="Employee Of the Month" {...a11yProps(3)} />
+        <Tab sx={{alignItems: 'flex-start', textTransform: 'none'}} label="Case Study" {...a11yProps(4)} />
+
+      </Tabs>
+      <TabPanel  value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        Item Four
+      </TabPanel>
+      <TabPanel value={value} index={4}>
+        Item Five
+      </TabPanel>
+    </Box>
+  );
+}
