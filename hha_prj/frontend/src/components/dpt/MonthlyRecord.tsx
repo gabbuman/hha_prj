88cefa6e4264d62c444ba0b3f822a01e3f6caeca
yunerc <yuner_cai@sfu.ca>
2021-10-10 17:16:29 -0700

    add an idention level for Box

--- hha_prj/frontend/src/components/dpt/MonthlyRecord.tsx
+++ hha_prj/frontend/src/components/dpt/MonthlyRecord.tsx
@@ -86,126 +86,126 @@ class DataForm extends Component <DFProps, DFState> {
             noValidate
             autoComplete="off"
             >
-            <Typography variant="h5">September Hospital Record</Typography>
-            <Box>
-            <Typography variant="h6">Overview</Typography>
-            <div>
-                <TextField
-                required
-                disabled={this.props.disabled}
-                id="outlined-required"
-                label="Beds Available"
-                value={this.state.bedsAvailable}
-                onChange={(e)=>{this.setState({bedsAvailable: e.target.value})}}
-                />
-                <TextField
-                required
-                disabled={this.props.disabled}
-                id=""
-                label="Bed Days"
-                value={this.state.bedDays}
-                onChange={(e)=>{this.setState({bedDays: e.target.value})}}
-                />
-                <TextField
-                required
-                disabled={this.props.disabled}
-                id=""
-                label="Patient Days"
-                value={this.state.patientDays}
-                onChange={(e)=>{this.setState({patientDays: e.target.value})}}
-                />
-                <TextField
-                required
-                disabled={this.props.disabled}
-                id=""
-                label="Hospitalized"
-                value={this.state.hospitalized}
-                onChange={(e)=>{this.setState({hospitalized: e.target.value})}}
-                />    
-                <TextField
-                required
-                disabled={this.props.disabled}
-                id=""
-                label="Discharged Alive"
-                value={this.state.dischargedAlive}
-                onChange={(e)=>{this.setState({dischargedAlive: e.target.value})}}
-                />          
-            </div>
-            </Box>
-            <Box>
-            <Typography variant="h6">Duration</Typography>
-            <div>
-                <TextField
-                required
-                disabled={this.props.disabled}
-                id="outlined-required"
-                label="Died Before 48hr"
-                value={this.state.diedBefore}
-                onChange={(e)=>{this.setState({diedBefore: e.target.value})}}
-                />
-                <TextField
-                required
-                disabled={this.props.disabled}
-                id=""
-                label="Died After 48hr"
-                value={this.state.diedAfter}
-                onChange={(e)=>{this.setState({diedAfter: e.target.value})}}
-                />
-                <TextField
-                required
-                disabled={this.props.disabled}
-                id=""
-                label="Days Hospitalized"
-                value={this.state.daysHospitalized}
-                onChange={(e)=>{this.setState({daysHospitalized: e.target.value})}}
-                />         
-            </div>
-            </Box>
-            <Box>
-            <Typography variant="h6">Movement</Typography>  
-            <div>
-                <TextField
-                required
-                disabled={this.props.disabled}
-                id="outlined-required"
-                label="Referrals"
-                value={this.state.referrals}
-                onChange={(e)=>{this.setState({referrals: e.target.value})}}
-                />
-                <TextField
-                required
-                disabled={this.props.disabled}
-                id=""
-                label="Transfers"
-                value={this.state.transfers}
-                onChange={(e)=>{this.setState({transfers: e.target.value})}}
-                />
-                <TextField
-                required
-                disabled={this.props.disabled}
-                id=""
-                label="Self-Discharged"
-                value={this.state.selfdischarged}
-                onChange={(e)=>{this.setState({selfdischarged: e.target.value})}}
-                />
-                <TextField
-                required
-                disabled={this.props.disabled}
-                id=""
-                label="Stayed in Ward"
-                value={this.state.stayedinward}
-                onChange={(e)=>{this.setState({stayedinward: e.target.value})}}
-                />  
-                <TextField
-                required
-                disabled={this.props.disabled}
-                id=""
-                label="Admissions"
-                value={this.state.admissions}
-                onChange={(e)=>{this.setState({admissions: e.target.value})}}
-                />          
-            </div>
-            </Box>
+                <Typography variant="h5">September Hospital Record</Typography>
+                <Box>
+                <Typography variant="h6">Overview</Typography>
+                <div>
+                    <TextField
+                    required
+                    disabled={this.props.disabled}
+                    id="outlined-required"
+                    label="Beds Available"
+                    value={this.state.bedsAvailable}
+                    onChange={(e)=>{this.setState({bedsAvailable: e.target.value})}}
+                    />
+                    <TextField
+                    required
+                    disabled={this.props.disabled}
+                    id=""
+                    label="Bed Days"
+                    value={this.state.bedDays}
+                    onChange={(e)=>{this.setState({bedDays: e.target.value})}}
+                    />
+                    <TextField
+                    required
+                    disabled={this.props.disabled}
+                    id=""
+                    label="Patient Days"
+                    value={this.state.patientDays}
+                    onChange={(e)=>{this.setState({patientDays: e.target.value})}}
+                    />
+                    <TextField
+                    required
+                    disabled={this.props.disabled}
+                    id=""
+                    label="Hospitalized"
+                    value={this.state.hospitalized}
+                    onChange={(e)=>{this.setState({hospitalized: e.target.value})}}
+                    />    
+                    <TextField
+                    required
+                    disabled={this.props.disabled}
+                    id=""
+                    label="Discharged Alive"
+                    value={this.state.dischargedAlive}
+                    onChange={(e)=>{this.setState({dischargedAlive: e.target.value})}}
+                    />          
+                </div>
+                </Box>
+                <Box>
+                <Typography variant="h6">Duration</Typography>
+                <div>
+                    <TextField
+                    required
+                    disabled={this.props.disabled}
+                    id="outlined-required"
+                    label="Died Before 48hr"
+                    value={this.state.diedBefore}
+                    onChange={(e)=>{this.setState({diedBefore: e.target.value})}}
+                    />
+                    <TextField
+                    required
+                    disabled={this.props.disabled}
+                    id=""
+                    label="Died After 48hr"
+                    value={this.state.diedAfter}
+                    onChange={(e)=>{this.setState({diedAfter: e.target.value})}}
+                    />
+                    <TextField
+                    required
+                    disabled={this.props.disabled}
+                    id=""
+                    label="Days Hospitalized"
+                    value={this.state.daysHospitalized}
+                    onChange={(e)=>{this.setState({daysHospitalized: e.target.value})}}
+                    />         
+                </div>
+                </Box>
+                <Box>
+                <Typography variant="h6">Movement</Typography>  
+                <div>
+                    <TextField
+                    required
+                    disabled={this.props.disabled}
+                    id="outlined-required"
+                    label="Referrals"
+                    value={this.state.referrals}
+                    onChange={(e)=>{this.setState({referrals: e.target.value})}}
+                    />
+                    <TextField
+                    required
+                    disabled={this.props.disabled}
+                    id=""
+                    label="Transfers"
+                    value={this.state.transfers}
+                    onChange={(e)=>{this.setState({transfers: e.target.value})}}
+                    />
+                    <TextField
+                    required
+                    disabled={this.props.disabled}
+                    id=""
+                    label="Self-Discharged"
+                    value={this.state.selfdischarged}
+                    onChange={(e)=>{this.setState({selfdischarged: e.target.value})}}
+                    />
+                    <TextField
+                    required
+                    disabled={this.props.disabled}
+                    id=""
+                    label="Stayed in Ward"
+                    value={this.state.stayedinward}
+                    onChange={(e)=>{this.setState({stayedinward: e.target.value})}}
+                    />  
+                    <TextField
+                    required
+                    disabled={this.props.disabled}
+                    id=""
+                    label="Admissions"
+                    value={this.state.admissions}
+                    onChange={(e)=>{this.setState({admissions: e.target.value})}}
+                    />          
+                </div>
+                </Box>
             </Box>
         )
     }
