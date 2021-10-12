8e9e76a90bf77b537fe9b205a96ebe062da5fb2c
yongqix <yongqi_xu@sfu.ca>
2021-10-11 22:49:13 -0700

    Put department cards into an adaptive grid.

--- hha_prj/frontend/src/components/home/Homepage.tsx
+++ hha_prj/frontend/src/components/home/Homepage.tsx
@@ -7,15 +7,40 @@ import CSPreview from '../home/CSPreview';
 import Rank from '../home/Rank';
 import {dpts_Data} from './DptData';
 import DptCard, { DptOverview } from './DptCard';
+import styled from 'styled-components'
 
+const CardGroup = styled.div `
+    margin: 40px;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 40px;
+    justify-items: center;
 
+    @media (max-width: 1220px) {
+        grid-template-columns: repeat(2, 1fr);
+    }
+
+    @media (max-width: 820px) {
+        grid-template-columns: repeat(1, 1fr);
+    }
+`
 
 class HomePage extends Component  {
     render() {
         return (
             <div>
                 <Header title={`Hope Health Action`} />
-                <DptOverview departmentData={dpts_Data}/>
+                <CardGroup>
+                    {dpts_Data.map(item => {
+                        return <DptCard  
+                            name={item.name}
+                            dpt_id={item.dpt_id}
+                            perc_of_data_entered={item.perc_of_data_entered}
+                            num_of_case_studies={item.num_of_case_studies}
+                            bg_img={item.bg_img}
+                            main_color={item.main_color}></DptCard>
+        })}
+                </CardGroup>
                 <Rank/>
                 <CSPreview/>
             </div>
