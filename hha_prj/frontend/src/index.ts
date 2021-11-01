3f96b527c35d0f6f2cf97eeec91c781b54fd3d43
vaibhavs <vaibhavs@sfu.ca>
2021-11-01 13:59:15 -0700

    convert frontend/index.js to index.ts

--- hha_prj/frontend/src/index.ts
+++ hha_prj/frontend/src/index.ts
@@ -1,13 +1,14 @@
-import App from './components/App';
 import express from "express";
 import { connectToDatabase } from "./database/index"
 import { csRouter } from "./routes/casestudy.router";
 
+const app = express();
+
 connectToDatabase()
     .then(() => {
-        App.use("/cs", csRouter);
+        app.use("/cs", csRouter);
 
-        App.listen('8080', () => {
+        app.listen('8080', () => {
             console.log("Server started at http://localhost:8080");
         });
     })
