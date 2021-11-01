29c48a6fe35c4dbbb6b1d81d0472d6751218af3d
vaibhavs <vaibhavs@sfu.ca>
2021-11-01 12:56:32 -0700

    Add mongodb connection to frontend

---
+++ hha_prj/frontend/src/database/index.ts
@@ -0,0 +1,20 @@
+import * as mongoDB from "mongodb";
+
+export const collections: {casestudy?: mongoDB.Collection } ={}
+
+const user = "admin";
+const userPassword = "pword";
+const cluster = "hhadb.tm3pa";
+const dbName = "HHA"
+
+const url = `mongodb+srv://${user}:${userPassword}@${cluster}.mongodb.net`;
+
+export async function connectToDatabase () {
+    
+    const client: mongoDB.MongoClient = new mongoDB.MongoClient(url);
+    await client.connect();
+    const db: mongoDB.Db = client.db(dbName);
+    console.log(`connect to database: ${dbName}`);
+
+    collections.casestudy = db.collection("Case_study");
+};
