f5dd4cf3c02689f4d97961af54842748170bd100
cta70 <calvin_truong@sfu.ca>
2021-11-03 23:31:38 -0700

    Updated citation for regex

--- hha_prj/frontend/src/components/login/FormValidation.tsx
+++ hha_prj/frontend/src/components/login/FormValidation.tsx
@@ -4,7 +4,7 @@ export const validateUsername = (username:string) => {
 }
 
 export const validatePassword = (password:string) => {
-    // Citation: https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
+    // Citation: https://www.thepolyglotdeveloper.com/2020/02/test-password-strength-regex-react-application/
     const regexp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[0-9a-zA-Z]{8,}$/;
     console.log(regexp.test(password));
     return regexp.test(password);
