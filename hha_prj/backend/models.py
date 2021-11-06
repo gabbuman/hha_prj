bab126e4a786e7f65a828ca0d2d3c922900d8fed
cta70 <calvin_truong@sfu.ca>
2021-11-06 13:07:51 -0700

    Added serializers, viewsets and tested storing a JSON array and retrieve it based on department FK

--- hha_prj/backend/models.py
+++ hha_prj/backend/models.py
@@ -488,8 +488,8 @@ class CustomUser(AbstractUser):
         return "%s %s" % (self.username, self.department)
 
 class CurrentFieldsList(models.Model):
-    list = models.JSONField(null=True,blank=True)
-    department = models.OneToOneField(Department, on_delete=models.PROTECT, default="Rehab")
+    list = models.JSONField(null=False,blank=False)
+    department = models.OneToOneField(Department, on_delete=models.PROTECT)
     created_at = models.DateTimeField(editable=False, auto_now_add=True)
     
     def __str__(self):
