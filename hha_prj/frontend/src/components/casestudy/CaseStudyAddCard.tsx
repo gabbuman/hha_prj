edd6a6c0d3d45e095d4d6ea4fc8d3d3f701117aa
shubhmall <skmall@sfu.ca>
2021-11-06 19:17:26 -0700

    Added background to the Add new case study card, make the case study grid page viewable from the tab and refactored code

--- hha_prj/frontend/src/components/casestudy/CaseStudyAddCard.tsx
+++ hha_prj/frontend/src/components/casestudy/CaseStudyAddCard.tsx
@@ -9,24 +9,36 @@ import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 
 const main_color = "#b7b5b6"
 
-const AddGrid = styled.div `
+const AddGrid = styled.button `
     width: 350px;
     height: 225px;
     padding: 15px 15px 15px 15px;
     box-shadow: 0 10px 20px ${main_color}77;
+    border: none;
+    outline: none;
     display: grid;
     grid-template-columns: 2fr 1fr;
+    border-radius: 10px;
     grid-gap: 0;
     margin: 0 0;
     justify-items: center;
     align-items: center;
-    
+    position: relative;
+    background-image: url("/static/37.svg");
 `
+
+const CardBackground = styled.img `
+    position: absolute;
+    top: 0; 
+    height: 100%;
+    z-index: -1;
+`
+
 
 function AddCSCard(){
     return(
         <AddGrid>
-            <p style={{fontSize:'60', font:'-moz-initial', fontWeight:'bold'}}>+ NEW</p>
+            <h3 style={{fontSize:'40px', fontWeight:'bold', cursor:'pointer'}}>+ NEW</h3>
         </AddGrid>
     )
 }
