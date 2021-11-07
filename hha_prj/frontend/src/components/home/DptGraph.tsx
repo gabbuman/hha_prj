bb6f71867b8802e6e760b71ffd8f37083f8ed543
yongqix <yongqi_xu@sfu.ca>
2021-11-07 01:10:48 -0800

    Resolve package conflicts.

--- hha_prj/frontend/src/components/home/DptGraph.tsx
+++ hha_prj/frontend/src/components/home/DptGraph.tsx
@@ -1,5 +1,14 @@
 import React, {useMemo, useCallback} from 'react';
 import {AreaClosed, Line, Bar} from '@visx/shape';
+import { curveMonotoneX } from '@visx/curve';
+import { GridRows, GridColumns } from '@visx/grid';
+import { scaleTime, scaleLinear } from '@visx/scale';
+import { withTooltip, Tooltip, TooltipWithBounds, defaultStyles } from '@visx/tooltip';
+import { WithTooltipProvidedProps } from '@visx/tooltip/lib/enhancers/withTooltip';
+import { localPoint } from '@visx/event';
+import { LinearGradient } from '@visx/gradient';
+import { max, extent, bisector } from 'd3-array';
+import { timeFormat } from 'd3-time-format';
 
 class DptGraphView extends React.Component {
     render() {
