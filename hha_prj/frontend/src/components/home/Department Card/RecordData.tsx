7a5a5c24138ce91c8a0e784e0ac298c9525cb4a9
yongqix <yongqi_xu@sfu.ca>
2021-11-08 13:12:00 -0800

    Add another set of sample data.

--- hha_prj/frontend/src/components/home/Department Card/RecordData.tsx
+++ hha_prj/frontend/src/components/home/Department Card/RecordData.tsx
@@ -41,3 +41,35 @@ export const sampleData: RecordDataSet = {
         {dateRecorded: "2021-12-24T07:00:00.000Z", value: 11},
     ]
 }
+
+export const sampleData2: RecordDataSet = {
+    recordType: "Bed Days",
+    startDate: "JAN 2020",
+    endDate: "JAN 2021",
+    data: [
+        {dateRecorded: "2020-01-24T07:00:00.000Z", value: 500},
+        {dateRecorded: "2020-02-24T07:00:00.000Z", value: 478},
+        {dateRecorded: "2020-03-24T07:00:00.000Z", value: 457},
+        {dateRecorded: "2020-04-24T07:00:00.000Z", value: 481},
+        {dateRecorded: "2020-05-24T07:00:00.000Z", value: 479},
+        {dateRecorded: "2020-06-24T07:00:00.000Z", value: 480},
+        {dateRecorded: "2020-07-24T07:00:00.000Z", value: 492},
+        {dateRecorded: "2020-08-24T07:00:00.000Z", value: 499},
+        {dateRecorded: "2020-09-24T07:00:00.000Z", value: 502},
+        {dateRecorded: "2020-10-24T07:00:00.000Z", value: 505},
+        {dateRecorded: "2020-11-24T07:00:00.000Z", value: 510},
+        {dateRecorded: "2020-12-24T07:00:00.000Z", value: 500},
+        {dateRecorded: "2021-01-24T07:00:00.000Z", value: 496},
+        {dateRecorded: "2021-02-24T07:00:00.000Z", value: 491},
+        {dateRecorded: "2021-03-24T07:00:00.000Z", value: 483},
+        {dateRecorded: "2021-04-24T07:00:00.000Z", value: 481},
+        {dateRecorded: "2021-05-24T07:00:00.000Z", value: 480},
+        {dateRecorded: "2021-06-24T07:00:00.000Z", value: 488},
+        {dateRecorded: "2021-07-24T07:00:00.000Z", value: 492},
+        {dateRecorded: "2021-08-24T07:00:00.000Z", value: 497},
+        {dateRecorded: "2021-09-24T07:00:00.000Z", value: 501},
+        {dateRecorded: "2021-10-24T07:00:00.000Z", value: 503},
+        {dateRecorded: "2021-11-24T07:00:00.000Z", value: 506},
+        {dateRecorded: "2021-12-24T07:00:00.000Z", value: 502},
+    ]
+}
