85847d097ae1495057809322425e4a9234952e7a
cta70 <calvin_truong@sfu.ca>
2021-11-08 15:08:31 -0800

    Swapped endpoint for vm

--- hha_prj/frontend/src/components/Endpoint.tsx
+++ hha_prj/frontend/src/components/Endpoint.tsx
@@ -1,5 +1,8 @@
-export const endpoint = "http://127.0.0.1:8000/"
+export const endpoint = "http://142.58.2.141:8000/"
 /*
     Use this endpoint instead when hosting on VM
     export const endpoint = "http://142.58.2.141:8000/"
+
+    Use this endpoint instead when developing locally
+    export const endpoint = "http://127.0.0.1:8000/"
 */
