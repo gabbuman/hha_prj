73653207ffc64b313ee5dde27ef92de3390e9b79
cta70 <calvin_truong@sfu.ca>
2021-11-24 00:35:10 -0800

    Added date error checking and httpbadrequest response

--- hha_prj/backend/views.py
+++ hha_prj/backend/views.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import permissions
 from django.http import HttpResponse
 from .serializers import CustomTokenPairSerializer
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 import datetime
 from .models import  MonthlyRecord
 
@@ -30,10 +30,15 @@ def CheckCurrentMonthAdmissionStatus(request):
     return HttpResponse(json.dumps(response), content_type="application/json")
 
 def GetRecordDataByDateRange(request, dept, min_year, min_month, max_year, max_month, field):
+
+    isValidDate = (min_year < max_year) or ((min_year is max_year) and (min_month < max_month))
+    if (not isValidDate):
+        return HttpResponseBadRequest("Invalid date range selected")
+
     record_list = list(MonthlyRecord.objects.filter(Q(year__gte=min_year), Q(month__gte=min_month),
         Q(year__lte=max_year), Q(month__lte=max_month), Q(department=dept)).values())
 
-    formatted_field = field.replace("$$$", " ") # Enable white space passing by an alias "$$$"
+    target_field = field.replace("$$$", " ") # Enable white space passing by an alias "$$$"
 
     response = []
     for dictionary in record_list:
@@ -42,13 +47,13 @@ def GetRecordDataByDateRange(request, dept, min_year, min_month, max_year, max_m
         date = datetime.date(year,month,1)
 
         question_answer_list = dictionary['question_answer_list']
-        question_answer_selection = [dictionary for dictionary in question_answer_list if formatted_field in dictionary.values()]
+        question_answer_selection = [dictionary for dictionary in question_answer_list if target_field in dictionary.values()]
         question_answer_pair = question_answer_selection[0]
         answer = question_answer_pair["answer"]
 
         response.append({ "date":date, "answer":answer })
 
-    question_and_response = {"question": formatted_field, "response" : response}
+    question_and_response = {"question": target_field, "response" : response}
 
     data = json.dumps(question_and_response,indent=4,sort_keys=True,default=str)
     return HttpResponse(data, content_type="application/json")
