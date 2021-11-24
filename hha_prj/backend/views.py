1f2412ac983b11baa463d5c59642cb60dbbea282
Manav <mjpatel@sfu.ca>
2021-11-23 20:23:09 -0800

    Implemented a function based view to help get the question list by itself for a specified department.

--- hha_prj/backend/views.py
+++ hha_prj/backend/views.py
@@ -5,13 +5,13 @@ from django.http import HttpResponse
 from .serializers import CustomTokenPairSerializer
 from django.http import HttpResponse
 from datetime import datetime
-from .models import  MonthlyRecord
+from .models import  CurrentFieldsList, MonthlyRecord
 
 from rest_framework.parsers import JSONParser
 from django.http.response import JsonResponse
 import json
 from rest_framework import viewsets
-
+from django.db.models import Q
 class ObtainTokenPairWithUsernameView(TokenObtainPairView):
     permission_classes = (permissions.AllowAny,)
     serializer_class = CustomTokenPairSerializer
@@ -26,3 +26,19 @@ def CheckCurrentMonthAdmissionStatus(request):
         response = False
 
     return HttpResponse(json.dumps(response), content_type="application/json")
+
+def GetCurrentFieldList(request,department_name):
+
+    response = []
+    
+    if (not CurrentFieldsList.objects.filter(department = department_name).exists()):
+        data = json.dumps(response)
+        return HttpResponse(data, content_type="application/json")
+
+    field_list = CurrentFieldsList.objects.filter(department = department_name).values()
+
+    question_list = field_list[0]['list']
+    response.append({"list": question_list})
+
+    data = json.dumps(response,indent=4,default=str)
+    return HttpResponse(data, content_type="application/json")
