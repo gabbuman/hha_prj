e44a142a06eef3d8a4a7bacff32e2fc59649b917
yongqix <yongqi_xu@sfu.ca>
2021-11-26 14:29:50 -0800

    Complete styling of the header.

--- hha_prj/frontend/src/components/layout/Header.tsx
+++ hha_prj/frontend/src/components/layout/Header.tsx
@@ -1,29 +1,30 @@
 import React, { ReactElement, FC } from 'react';
+import './Header.css'
 
 interface Props {
     title: string
 }
 
-const Header: FC<Props> = ({title}): ReactElement => {
-    return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <img src="/static/logo.png" width="60" height="50"
-                     className="d-inline-block align-top" alt="hha logo"/>
-                <a className="navbar-brand" href="#">{title}</a>
-            </nav>
-        </div>
-    )
-}
+// const Header: FC<Props> = ({title}): ReactElement => {
+//     return (
+//         <div>
+//             <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//                 <img src="/static/logo.png" width="60" height="50"
+//                      className="d-inline-block align-top" alt="hha logo"/>
+//                 <a className="navbar-brand" href="#">{title}</a>
+//             </nav>
+//         </div>
+//     )
+// }
 
-export default Header
+// export default Header
 
 interface HeaderState {
     hasScrolled: boolean;
     title: string;
 }
 
-class AnimatedHeader extends React.Component<Props, HeaderState> {
+class Header extends React.Component<Props, HeaderState> {
     constructor(props: Props) {
         super(props)
 
@@ -47,14 +48,14 @@ class AnimatedHeader extends React.Component<Props, HeaderState> {
             <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
                 <div className="HeaderGroup">
                     <img src="/static/logo.png" 
-                         width="60" height="50"
+                         width="30" height="25"
                          className="d-inline-block align-top" 
                          alt="hha logo"/>
-                    <a className="navbar-brand" href="#">{this.state.title}</a>
+                    <a className="TitleLabel" href="#">{this.state.title}</a>
                 </div>
             </div>
         )
     }
 }
 
-// export default Header
+export default Header
