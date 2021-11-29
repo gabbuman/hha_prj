f1b0cf0408d51790050b078180319580a82cf388
yunerc <yunerc@sfu.ca>
2021-11-28 22:05:36 -0800

    [issue 44] dynamically generate list and change input works

--- hha_prj/frontend/src/components/questions/QuestionList.tsx
+++ hha_prj/frontend/src/components/questions/QuestionList.tsx
@@ -8,6 +8,7 @@ import { ThreeSixtyTwoTone } from '@mui/icons-material';
 import { green, orange } from '@mui/material/colors';
 import AddBoxIcon from '@mui/icons-material/AddBox';
 import IndeterminateCheckBoxIcon from '@mui/icons-material/IndeterminateCheckBox';
+import { SentimentSatisfiedAltSharp } from '@material-ui/icons';
 
 
 interface QLProps {
@@ -16,13 +17,25 @@ interface QLProps {
 
 interface QLState {
     dptName: string;
-    questions: string[];
+    questions: {[key: string]: any}[];
     // red_data_shared: {[key:string]: number};
 }
 
 
 const QuestionList = (props: QLProps, state: QLState) => {
     const [dptName, setDptName] = useState(props.dptName);
+    const [questions, setQuestions] = useState([{
+        id: 1,
+        question: ""
+    }])
+
+    const handleChangeInput = (i: number, e: React.ChangeEvent<HTMLInputElement>) => {
+        // console.log(e.target.name);
+        const values:any = [...questions]
+        // console.log(values[i])
+        values[i][e.target.name] = e.target.value
+        setQuestions(values)
+    }
 
     const addClick: any = () => {
 
@@ -50,20 +63,29 @@ const QuestionList = (props: QLProps, state: QLState) => {
 
                             <Form>
                                 <FormGroup>
-                                    <Row className="mt-5">
-                                        <Col md>
-                                            <Form.Label>Question</Form.Label>
-                                            <Form.Control type="text" placeholder="Enter question" />
-                                        </Col>
-                                        <Col md>
-                                            <IconButton className="mt-4" sx={{ color: green[500] }} onClick={() => addClick()}>
-                                                <AddBoxIcon/>
-                                            </IconButton>
-                                            <IconButton className="mt-4" sx={{ color: orange[500] }} onClick={() => removeClick()}>
-                                                <IndeterminateCheckBoxIcon/>
-                                            </IconButton>
-                                        </Col>
-                                    </Row>
+                                    {questions.map((question, i) => (
+                                        <div key={question.id}>
+                                            <Row className="mt-5">
+                                                <Col md>
+                                                    <Form.Label>Question</Form.Label>
+                                                    <Form.Control type="text" 
+                                                    placeholder="Enter question"
+                                                    name = "question"
+                                                    value={question.question}
+                                                    onChange={e => handleChangeInput(i, e as any)} 
+                                                    />
+                                                </Col>
+                                                <Col md>
+                                                    <IconButton className="mt-4" sx={{ color: green[500] }} onClick={() => addClick()}>
+                                                        <AddBoxIcon/>
+                                                    </IconButton>
+                                                    <IconButton className="mt-4" sx={{ color: orange[500] }} onClick={() => removeClick()}>
+                                                        <IndeterminateCheckBoxIcon/>
+                                                    </IconButton>
+                                                </Col>
+                                            </Row>
+                                        </div>
+                                    ))}
                                 </FormGroup>
                             </Form>
                
