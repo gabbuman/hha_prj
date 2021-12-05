e8ef8f1353b10e3b03a06a6060e6ccc0e56dabb8
cta70 <calvin_truong@sfu.ca>
2021-12-04 21:11:49 -0800

    Converted ProtectedRoutes.js to work in typescript

--- hha_prj/frontend/src/protected-routes/ProtectedRoutes.tsx
+++ hha_prj/frontend/src/protected-routes/ProtectedRoutes.tsx
@@ -1,13 +1,14 @@
 import { Route, Redirect } from "react-router-dom";
-import { useAuthContext } from "@contexts/AuthContext";
-import { useUserContext } from "@contexts/UserContext";
+import { useAuthContext } from "./../contexts/AuthContext";
+import { useUserContext } from "./../contexts/UserContext";
 import Error from "../pages/Error";
+import React from 'react'
 
 export default function ProtectedRoutes({
   component: Component,
   template,
   ...rest
-}) {
+}:any) {
   const { auth } = useAuthContext();
   const { user } = useUserContext();
 
