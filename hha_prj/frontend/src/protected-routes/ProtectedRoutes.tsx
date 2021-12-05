1af5e990f8257fd4f0a1f9535226f790119a26d2
cta70 <calvin_truong@sfu.ca>
2021-12-04 21:10:55 -0800

    Copied over protected routes implementation from previous project

---
+++ hha_prj/frontend/src/protected-routes/ProtectedRoutes.tsx
@@ -0,0 +1,56 @@
+import { Route, Redirect } from "react-router-dom";
+import { useAuthContext } from "@contexts/AuthContext";
+import { useUserContext } from "@contexts/UserContext";
+import Error from "../pages/Error";
+
+export default function ProtectedRoutes({
+  component: Component,
+  template,
+  ...rest
+}) {
+  const { auth } = useAuthContext();
+  const { user } = useUserContext();
+
+  const switchRoute = () => {
+    switch (template) {
+      case "accessibleAfterLogin":
+        return (
+          <Route
+            {...rest}
+            render={(props) => {
+              return auth || user ? (
+                <Component {...props} />
+              ) : (
+                <Error msg={"Not logged in"} />
+              );
+            }}
+          />
+        );
+
+      case "accessibleBeforeLogin":
+        return (
+          <Route
+            {...rest}
+            render={(props) => {
+              return !auth && !user ? (
+                <Component {...props} />
+              ) : (
+                <Redirect
+                  to={{
+                    pathname: "/",
+                    state: {
+                      from: props.location,
+                    },
+                  }}
+                />
+              );
+            }}
+          />
+        );
+      default:
+        return;
+    }
+  };
+
+  return <>{switchRoute()}</>;
+}
